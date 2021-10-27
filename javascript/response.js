feaece5452ab4f016b7aa65828680ae6add49b8e
Decclan Lawrence <declan26@gmail.com>
2021-10-27 13:22:53 +0200

    testing js

---
+++ javascript/response.js
@@ -0,0 +1,8 @@
+function burger() {
+    var x = document.getElementById("navigationbar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
